338cdafe1c0b1f89f32d985bcc52c6a3be1fc418
gideonsmila <gideonsmila@gmail.com>
2024-08-11 15:08:44 +0300

    change app subject

--- app/database/models.py
+++ app/database/models.py
@@ -1,15 +1,9 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Enum as SQLAEnum
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import DeclarativeBase
-import enum
 
 class Base(DeclarativeBase):
     pass
-
-class SnakeType(enum.Enum):
-    python = "python"
-    cobra = "cobra"
-    viper = "viper"
 
 class User(Base):
     __tablename__ = "user"
@@ -17,15 +11,16 @@ class User(Base):
     hash_pwd = Column(String, nullable=False)
     name = Column(String, nullable=False)
 
-class Snake(Base):
+class Design(Base):
+    __tablename__ = 'design'
+    id = Column(Integer, primary_key= True)
+    comments = relationship('Comment', back_populates='design')
+
+class Comment(Base):
     __tablename__ = 'snake'
     id = Column(Integer, primary_key=True)
-    type = Column(SQLAEnum(SnakeType), nullable=False)
-    tank_id = Column(Integer, ForeignKey('tank.id'), nullable=False)
-    tank = relationship('Tank', back_populates='snakes')
+    design_id = Column(Integer, ForeignKey('design.id'), nullable=False)
+    design = relationship('Design', back_populates='comments')
 
-class Tank(Base):
-    __tablename__ = 'tank'
-    id = Column(Integer, primary_key= True)
-    snakes = relationship('Snake', back_populates='tank')
+
 
